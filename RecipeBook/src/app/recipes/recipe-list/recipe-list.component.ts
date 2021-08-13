1ca007df0b1d07f4103b17eaa6292bedbd0fe5b3
GeneralTaoChikn <chrisdiasanta@gmail.com>
2021-08-13 09:27:43 -0400

    edited recipe and shopping components

--- RecipeBook/src/app/recipes/recipe-list/recipe-list.component.ts
+++ RecipeBook/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Sugar Water', 'Thiss is simply coolaid', 
+    'https://images.heb.com/is/image/HEBGrocery/000136081?fit=constrain,1&wid=800&hei=800&fmt=jpg&qlt=85,0&resMode=sharp2&op_usm=1.75,0.3,2,0')
+  ];
 
   constructor() { }
 
--- RecipeBook/src/app/shopping-list/shopping-list.component.ts
+++ RecipeBook/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ingridient } from '../shared/ingredient.model';
+
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingridients: ingridient[] =[
+    new ingridient('Apples',5),
+    new ingridient('wheat',5),
+  ];
   constructor() { }
 
   ngOnInit(): void {
