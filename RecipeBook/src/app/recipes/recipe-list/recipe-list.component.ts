37cd773013d6a1aefdbc9bab0bf6d8287dcf4732
GeneralTaoChikn <chrisdiasanta@gmail.com>
2021-08-16 16:35:56 -0400

    added click feature to navigations

--- RecipeBook/src/app/recipes/recipe-list/recipe-list.component.ts
+++ RecipeBook/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,6 +9,8 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('Sugar Water', 'Thiss is simply coolaid', 
+    'https://images.heb.com/is/image/HEBGrocery/000136081?fit=constrain,1&wid=800&hei=800&fmt=jpg&qlt=85,0&resMode=sharp2&op_usm=1.75,0.3,2,0'),
+    new Recipe('Sugar Water', 'Thiss is simply coolaid', 
     'https://images.heb.com/is/image/HEBGrocery/000136081?fit=constrain,1&wid=800&hei=800&fmt=jpg&qlt=85,0&resMode=sharp2&op_usm=1.75,0.3,2,0')
   ];
 
